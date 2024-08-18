eaffc310445ad448d6449596858a3d86fc280ab1
CreaDX <simpledan222@gmail.com>
2024-08-18 13:35:23 +0300

    Update script.js

--- script.js
+++ script.js
@@ -31,14 +31,10 @@ document.addEventListener("DOMContentLoaded", function() {
     console.log(`Circle radius updated to: ${newRadius}m`);
   });
 
-window.onload = function() {
-  console.log('Window loaded!');
-  var videoElement = document.getElementById('reload-video');
-  console.log('Video element:', videoElement);
-  videoElement.classList.add('reload-video-show');
-  console.log('Added reload-video-show class');
-  setTimeout(function() {
-    videoElement.classList.remove('reload-video-show');
-    console.log('Removed reload-video-show class');
-  }, 7000); // hide the video after 4 seconds
-};
+  // Show the video animation on page load
+  window.onload = function() {
+    console.log('Window loaded!');
+    var videoElement = document.getElementById('reload-video');
+    console.log('Video element:', videoElement);
+    videoElement.classList.add('reload-video-show');
+    console.log('Added reload
